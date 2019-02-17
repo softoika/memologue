483188a47b4581404ea206117dd1e4b1bd4a7eb6
Ryo Hanafusa <zwgntz@gmail.com>
2019-02-17 18:09:51 +0900

    AppComponentにaddMessage(message: string)を追加

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'memologue'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('memologue');
+  /**
+   * コンポーネントインスタンスのaddMessageメソッドで複数のメッセージの追加・取得ができることを確認する。
+   */
+  it('should add 2 messages', () => {
+    const component = new AppComponent();
+    const m1 = 'hogehoge';
+    const m2 = 'fugafuga';
+    component.addMessage(m1);
+    component.addMessage(m2);
+    expect(component.messages).toEqual([m1, m2]);
   });
 
-  it('should render title in a h1 tag', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to memologue!');
-  });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'memologue';
+
+  messages: string[] = [];
+
+  addMessage(message: string) {
+    this.messages.push(message);
+  }
+
 }
